959cf826682164e5d1747a058fd8bd6cce661d6d
Blessing Udeji <budeji@gmail.com>
2024-01-22 02:01:00 +0100

    Markdown2html

---
+++ markdown2html.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+A Script that converts a Markdown file to HTML.
+"""
+import argparse
+import pathlib
+import re
+
+
+def convert_md_to_html(input_file, output_file):
+    '''
+    Converts markdown file to HTML file
+    '''
+    with open(input_file, encoding='utf-8') as f:
+        md_content = f.readlines()
+
+    html_content = []
+    for line in md_content:
+        match = re.match(r'(#){1,6} (.*)', line)
+        if match:
+            h_level = len(match.group(1))
+            h_content = match.group(2)
+            html_content.append(f'<h{h_level}>{h_content}</h{h_level}>\n')
+        else:
+            html_content.append(line)
+
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.writelines(html_content)
+
+
+if __name__ == '__main__':
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Convert markdown to HTML')
+    parser.add_argument('input_file', help='path to input markdown file')
+    parser.add_argument('output_file', help='path to output HTML file')
+    args = parser.parse_args()
+
+    input_path = pathlib.Path(args.input_file)
+    if not input_path.is_file():
+        print(f'Missing {input_path}', file=sys.stderr)
+        sys.exit(1)
+
+    # Convert the markdown file to HTML
+    convert_md_to_html(args.input_file, args.output_file)
